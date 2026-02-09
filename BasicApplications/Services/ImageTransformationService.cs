c59f34538f5e4337761caa97417b62c897c6d51e
Sivateja Pathipati <pathipati.sivateja@gmail.com>
2026-02-09 19:59:46 +0530

    Add new service for image transformations

---
+++ BasicApplications/Services/ImageTransformationService.cs
@@ -0,0 +1,32 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicApplications.Services
+{
+    internal class ImageTransformationService
+    {
+
+        public static MagickImage GenerateThumbnail(MagickImage image, int maxSize = 200)
+        {
+            MagickImage img = (MagickImage)image.Clone();
+            img.AutoOrient();
+            img.FilterType = FilterType.Lanczos;
+
+            MagickGeometry geometry = new MagickGeometry((uint)maxSize, (uint)maxSize)
+            {
+                IgnoreAspectRatio = false,
+                Greater = false
+            };
+            img.Resize(geometry);
+            img.Extent((uint)maxSize, (uint)maxSize, Gravity.Center, MagickColors.White);
+            img.Quality = 75;
+            img.Strip();
+
+            return img;
+        }
+    }
+}
